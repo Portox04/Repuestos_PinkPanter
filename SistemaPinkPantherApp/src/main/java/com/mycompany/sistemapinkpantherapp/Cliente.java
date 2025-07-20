b4adac8d51d98888e0dab5a5aebf919386efd904
Portox04 <dibtecportocarrero@gmail.com>
2025-07-19 20:31:47 -0600

    Parte usuarios y ventas con su interfaz

--- SistemaPinkPantherApp/src/main/java/com/mycompany/sistemapinkpantherapp/Cliente.java
+++ SistemaPinkPantherApp/src/main/java/com/mycompany/sistemapinkpantherapp/Cliente.java
@@ -8,12 +8,16 @@ package com.mycompany.sistemapinkpantherapp;
  *
  * @author HP
  */
-public class Cliente {
+public class Cliente extends Usuario{
 
     private int idCliente;
     private String nombre;
     private String cedula;
     private String telefono;
     private double creditoRestante;
+
+    public Cliente(int idUsuario, String nombre, String rol, String correo, String clave) {
+        super(idUsuario, nombre, rol, correo, clave);
+    }
 
 }
